632b14ceb2a0c9ca76c4fc56fd98b4e44cb0da22
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-03-29 12:45:20 +0545

    Chat Model

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
 
-class Chat extends Model
-{
-    use HasFactory;
-}
+    class Chat extends Model
+    {
+        use HasFactory;
+
+        protected $fillable = ['user_id', 'context'];
+
+        protected $casts = [
+            'context' => 'array',
+        ];
+    }
